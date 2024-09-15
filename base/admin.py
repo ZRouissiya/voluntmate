e79043879f7643100a5e85e1e52d574852e11938
DarkShadowG17 <rouissiyazh@gmail.com>
2024-09-15 17:57:00 +0100

    added main Models ( User, Type , Post, Message)

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post,Type,Message,User
+
+admin.site.register(User)
+admin.site.register(Type)
+admin.site.register(Message)
+admin.site.register(Post)
--- base/models.py
+++ base/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    SEXE_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female')
+    ]
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=False)
+    age = models.IntegerField(null=True)
+    gendre=models.CharField(max_length=1,choices=SEXE_CHOICES,default='M',null=False)
+    avatar = models.ImageField(null=True, default="avatar.svg")
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+class Type(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    type = models.ForeignKey(Type, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.body[0:50]
